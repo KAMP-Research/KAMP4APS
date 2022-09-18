ba7b29dfa62cfdf6c45851ce9ca4a2e86ba30957
Sandro <layornos@gmail.com>
2022-09-18 16:11:32 +0200

    eval(scenario04): make changes for the fourth scenario

--- edu.kit.ipd.sdq.kamp4aps/src/edu/kit/ipd/sdq/kamp4aps/core/scenarios/ScrewingChanges.java
+++ edu.kit.ipd.sdq.kamp4aps/src/edu/kit/ipd/sdq/kamp4aps/core/scenarios/ScrewingChanges.java
@@ -9,50 +9,36 @@ import java.util.Set;
 import edu.kit.ipd.sdq.kamp4aps.core.APSArchitectureModelLookup;
 import edu.kit.ipd.sdq.kamp4aps.core.APSArchitectureVersion;
 import edu.kit.ipd.sdq.kamp4aps.core.ModifyComponentHandler;
+import edu.kit.ipd.sdq.kamp4aps.core.ModifyInterfaceHandler;
 import edu.kit.ipd.sdq.kamp4aps.core.changepropagation.InterfaceChanges;
 import edu.kit.ipd.sdq.kamp4aps.model.KAMP4aPSModificationmarks.ChangePropagationDueToHardwareChange;
-import edu.kit.ipd.sdq.kamp4aps.model.KAMP4aPSModificationmarks.KAMP4aPSModificationmarksFactory;
-import edu.kit.ipd.sdq.kamp4aps.model.KAMP4aPSModificationmarks.ModifyComponent;
-import edu.kit.ipd.sdq.kamp4aps.model.KAMP4aPSModificationmarks.ModifyInterface;
 import edu.kit.ipd.sdq.kamp4aps.model.KAMP4aPSModificationmarks.ModifyModule;
 import edu.kit.ipd.sdq.kamp4aps.model.aPS.ComponentRepository.Component;
 import edu.kit.ipd.sdq.kamp4aps.model.aPS.InterfaceRepository.Interface;
-import edu.kit.ipd.sdq.kamp4aps.model.aPS.InterfaceRepository.Screwing;
 import edu.kit.ipd.sdq.kamp4aps.model.aPS.ModuleRepository.Module;
 
 public class ScrewingChanges extends InterfaceChanges {
 
-	private Collection<Screwing> initialMarkedScrewings;
+	private Collection<Interface> initialMarkedScrewings;
 	private Map<Interface, Set<Component>> componentsToBeMarked;
 	private Map<Interface, Set<Module>> modulesToBeMarked;
 
 	public ScrewingChanges(APSArchitectureVersion v) {
 		super(v);
-		initialMarkedScrewings = APSArchitectureModelLookup.lookUpMarkedObjectsOfAType(version, Screwing.class);
+		initialMarkedScrewings = APSArchitectureModelLookup.lookUpMarkedObjectsOfAType(version, Interface.class);
 		componentsToBeMarked = new HashMap<Interface, Set<Component>>();
 		modulesToBeMarked = new HashMap<Interface, Set<Module>>();
 	}
 
 	public void addInitialMarkedScrewingsToList(
 			ChangePropagationDueToHardwareChange changePropagationDueToHardwareChange) {
-		for(Screwing screwing : initialMarkedScrewings){
-			ModifyInterface<Interface> modifyScrewing = KAMP4aPSModificationmarksFactory.eINSTANCE.createModifyInterface();
-			modifyScrewing.setAffectedElement(screwing);
-			modifyScrewing.setToolderived(true);
-			modifyScrewing.setId("Modify" + screwing.getId());
-			changePropagationDueToHardwareChange.getInterfaceModifications().add(modifyScrewing);
+		for(Interface screwing : initialMarkedScrewings){
+			ModifyInterfaceHandler.createNewModifyInterface(screwing, changePropagationDueToHardwareChange);
 		}
 	}
 	
 	public void calculateAndMarkAffectedComponentsByScrewingChange(ChangePropagationDueToHardwareChange changePropagationDueToHardwareChange){
-		List<ModifyComponent<Component>> modifyComponents = null;
-		int mapHash;
-		do {
-			mapHash = componentsToBeMarked.hashCode();
-			componentsToBeMarked = APSArchitectureModelLookup.lookUpParentComponentsOfInterfaces(initialMarkedScrewings, changePropagationDueToHardwareChange);
-			modifyComponents = ModifyComponentHandler.createModifyComponentsFromAffectedComponents(componentsToBeMarked);
-			ModifyComponentHandler.addToModifyComponentsToChangePropagation(modifyComponents, changePropagationDueToHardwareChange);
-		} while (mapHash != componentsToBeMarked.hashCode());
+		ModifyComponentHandler.initModifyComponentsBasedOnInterfaces(componentsToBeMarked, changePropagationDueToHardwareChange, initialMarkedScrewings);
 	}
 	
 	public void calclulateAndMarkAffectedModulesByScrewingChange(ChangePropagationDueToHardwareChange changePropagationDueToHardwareChange){
